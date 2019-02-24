cba42f2718056bc3bf8177b2d2f188f210d0bc3f
Carlos Rangel <carlos.developer17@gmail.com>
2019-02-24 16:28:11 -0600

    (Unfinished) Began working on dashboard component

--- src/app/_components/dashboard-menu-box/dashboard-menu-box.component.ts
+++ src/app/_components/dashboard-menu-box/dashboard-menu-box.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-menu-box',
   templateUrl: './dashboard-menu-box.component.html',
-  styleUrls: ['./dashboard-menu-box.component.css']
+  styleUrls: ['./dashboard-menu-box.component.scss']
 })
-export class DashboardMenuBoxComponent implements OnInit {
+export class DashboardMenuBoxComponent {
+
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() links: string;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface SectionLink {
+  name: string;
+  route: string;
+}
+export interface Section {
+  title: string;
+  subtitle: string;
+  links: SectionLink[];
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  title = 'Template Application';
-  panelOpenState = false;
+  company: Section = { title: 'Company', subtitle: 'Supplier', links: [
+    { name: 'Go To Market', route: '/market' },
+    { name: 'Go To Dashboard', route: '/dashboard' }
+  ]};
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
